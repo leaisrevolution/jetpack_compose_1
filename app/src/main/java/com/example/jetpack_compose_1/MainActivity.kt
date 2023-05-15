087a784c290d8139ae552a9f7edf40cdfe493aca
leaisrevolution <leaisrevolution@gmail.com>
2023-05-15 14:54:59 +0900

    Add : 2nd page

--- app/src/main/java/com/example/jetpack_compose_1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetpack_compose_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
@@ -12,16 +13,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpack_compose_1.ui.theme.Jetpack_compose_1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           HomeScreen()
+//           HomeScreen()
+            ResultScreen(bmi = 35.0)
         }
     }
 }
@@ -67,4 +73,38 @@ fun HomeScreen() {
             }
         }
     }
+}
+
+@Composable
+fun ResultScreen(bmi: Double) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {Text(text = "비만도 계산기")}
+            )
+        }
+    ) {
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ){
+            Text("과체중", fontSize = 30.sp)
+            Spacer(modifier = Modifier.height(50.dp))
+            Image(
+                painter = painterResource(id = R.drawable.baseline_sentiment_dissatisfied_24),
+                contentDescription = null,
+                modifier = Modifier.size(100.dp),
+                colorFilter = ColorFilter.tint(
+                     color = Color.Black,
+                )
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Preview() {
+    ResultScreen(bmi = 35.0)
 }
