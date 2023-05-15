5da26533b3e8a4dce895596c8d34bd350571ef5f
leaisrevolution <leaisrevolution@gmail.com>
2023-05-15 11:27:18 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jetpack_compose_1/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.jetpack_compose_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpack_compose_1.ui.theme.Jetpack_compose_1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+           HomeScreen()
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Scaffol(
+        topBar = {
+            TopAppBar(
+                title = {Text(text = "비만도 계산기")}
+            )
+        }
+    ) {
+        Column {
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text(text = "키")},
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text(text = "몸무게")},
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = {}
+            ) {
+              Text("결과")
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Jetpack_compose_1Theme {
+        Greeting("Android")
+    }
+}
