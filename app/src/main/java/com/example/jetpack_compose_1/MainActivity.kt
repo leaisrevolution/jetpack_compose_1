8c627bd3494a473bcd442517e2297e5f5e439fdf
leaisrevolution <leaisrevolution@gmail.com>
2023-05-15 11:31:21 +0900

    Edit : Err

--- app/src/main/java/com/example/jetpack_compose_1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_1/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,7 +25,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreen() {
-    Scaffol(
+    Scaffold(
         topBar = {
             TopAppBar(
                 title = {Text(text = "비만도 계산기")}
@@ -47,17 +48,12 @@ fun HomeScreen() {
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             )
             Spacer(modifier = Modifier.height(8.dp))
-            Button(onClick = {}
+            Button(
+                onClick = {},
+                modifier = Modifier.align(Alignment.End),
             ) {
               Text("결과")
             }
         }
-    }
-}
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Jetpack_compose_1Theme {
-        Greeting("Android")
     }
 }
